eb0c891fcbf623ac57a11d810d84649389469fad
wilson20042025 <wilso20042025@gmail.com>
2026-03-03 00:27:53 +0530

    new update: update1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,12 @@ import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const prismaClientSingleton = () => {
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+    const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
     const adapter = new PrismaPg(pool)
     return new PrismaClient({ adapter })
 }
