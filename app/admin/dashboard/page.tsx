be62ba898aa9c9a780e759293c7a11386bd9d9b7
wilson20042025 <wilso20042025@gmail.com>
2026-03-03 01:00:31 +0530

    fixed: admindashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from 'next/link'
 import prisma from '@/lib/prisma'
 import { Plus, Edit, Trash2, LayoutDashboard } from 'lucide-react'
